67ad89583867a43c615e56c8f667733dba77e592
shuler7 <vitalisobolevggg@gmail.com>
2025-12-02 22:36:16 +0300

    kt-34: add base template of authors page

--- frontend/src/pages/AuthorsPage.jsx
+++ frontend/src/pages/AuthorsPage.jsx
@@ -2,8 +2,8 @@ import './AuthorsPage.css'
 
 export default function AuthorsPage() {
     return (
-        <div id='authors-page-container'>
-
+        <div id="authors-page-container" className='page-container'>
+            <img id="mai-logo" src="/images/mai-logo-color.png" />
         </div>
     )
 }
